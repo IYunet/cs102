1986bfec0a39b38ef4f4b5b4d0722058eaaf4e86
Ivan Kozlov <69389177+IYunet@users.noreply.github.com>
2021-04-06 17:48:27 +0300

    Isort#5

--- homework03/life_gui.py
+++ homework03/life_gui.py
@@ -8,7 +8,7 @@ class GUI(UI):
     def __init__(self, life: GameOfLife, cell_size: int = 10, speed: int = 10) -> None:
         super().__init__(life)
         self.cell_size = cell_size
-         self.screen = pygame.display.set_mode(
+        self.screen = pygame.display.set_mode(
             (self.life.cols * self.cell_size, self.life.rows * self.cell_size)
         )
         self.speed = speed  
