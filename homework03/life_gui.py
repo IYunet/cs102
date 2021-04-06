5118aabf3aa7ddaaf97a27b8516e855f65f90e01
Ivan Kozlov <69389177+IYunet@users.noreply.github.com>
2021-04-06 18:13:39 +0300

    Black correction#2

--- homework03/life_gui.py
+++ homework03/life_gui.py
@@ -7,11 +7,10 @@ from ui import UI
 class GUI(UI):
     def __init__(self, life: GameOfLife, cell_size: int = 10, speed: int = 10) -> None:
         super().__init__(life)
-        self.cell_size = cell_size
         self.screen = pygame.display.set_mode(
             (self.life.cols * self.cell_size, self.life.rows * self.cell_size)
         )
-        
+        self.cell_size = cell_size
         self.speed = speed  
 
     def draw_lines(self) -> None:
