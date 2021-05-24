85a2e44ec308b1d85cdc368d82587a93889e7daa
IYunet <lbvfzx@gmail.com>
2021-05-24 18:25:32 +0300

    Добавлен токен

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,6 +2,6 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "",
+    "access_token": "8624de97a48cd4a8a7e1b3bc3e32ccac142e9b406c47a28ee61c2e8df6bfde108e4629a8c18a349d11205",
     "version": "5.126",
 }
