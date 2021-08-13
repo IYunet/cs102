dfc6572ca06cd3f274d3c41eadc93d344945707b
IYunet <lbvfzx@gmail.com>
2021-08-13 14:36:47 +0800

    Part_of_8HW_2

--- homework08-web/wsgiserver/request.py
+++ homework08-web/wsgiserver/request.py
@@ -1,23 +1,19 @@
 import dataclasses
+import io
 import sys
 import typing as tp
 
-
-@dataclasses.dataclass
-class HTTPRequest:
-    method: bytes
-    url: bytes
-    headers: tp.Dict[bytes, bytes]
-    body: bytes
+from httpserver import HTTPRequest
 
 
 @dataclasses.dataclass
 class WSGIRequest(HTTPRequest):
     def to_environ(self) -> tp.Dict[str, tp.Any]:
         environ = {}
+
         environ["REQUEST_METHOD"] = self.method.decode()
         environ["SCRIPT_NAME"] = ""
-        environ["PATH_INFO"] = self.url.decode()
+        environ["PATH_INFO"] = self.url.decode("utf-8")
         environ["QUERY_STRING"] = (
             self.url.decode("utf-8").split("?")[1] if "?" in self.url.decode() else ""
         )
@@ -30,7 +26,7 @@ class WSGIRequest(HTTPRequest):
         environ["SERVER_PROTOCOL"] = "HTTP/1.1"
         environ["wsgi.version"] = (1, 0)
         environ["wsgi.url_scheme"] = "https" if self.url.startswith(b"https") else "http"
-        environ["wsgi.input"] = sys.stdin
+        environ["wsgi.input"] = io.BytesIO(self.body)
         environ["wsgi.errors"] = sys.stderr
         environ["wsgi.multithread"] = True
         environ["wsgi.multiprocess"] = False
@@ -38,4 +34,5 @@ class WSGIRequest(HTTPRequest):
 
         for header in self.headers:
             environ[f"HTTP_{header.decode().upper()}"] = self.headers[header].decode()
+
         return environ
